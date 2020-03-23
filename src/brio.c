5063f84fbc8e25112663b8da159e0d11a3f59a7a
Jim Hester <james.f.hester@gmail.com>
2020-03-23 10:07:19 -0400

    Simplify conditional in helper function

--- src/brio.c
+++ src/brio.c
@@ -6,27 +6,29 @@
 #endif
 
 // This is needed to support wide character paths on windows
-FILE* open_with_widechar_on_windows(const char* txt, const char* mode) {
+FILE* open_with_widechar_on_windows(const char* path, const char* mode) {
   FILE* out;
 #ifdef _WIN32
+  // First conver the mode to the wide equivalent
+  // Only usage is 2 characters so max 8 bytes + 2 byte null.
+  wchar_t mode_w[10];
+  MultiByteToWideChar(CP_UTF8, 0, mode, -1, mode_w, 9);
+  
+  // Then convert the path
   wchar_t* buf;
-  size_t len = MultiByteToWideChar(CP_UTF8, 0, txt, -1, NULL, 0);
+  size_t len = MultiByteToWideChar(CP_UTF8, 0, path, -1, NULL, 0);
   if (len <= 0) {
-    error("Cannot convert file to Unicode: %s", txt);
+    error("Cannot convert file to Unicode: %s", path);
   }
   buf = (wchar_t*)R_alloc(len, sizeof(wchar_t));
   if (buf == NULL) {
     error("Could not allocate buffer of size: %ll", len);
   }
 
-  MultiByteToWideChar(CP_UTF8, 0, txt, -1, buf, len);
-  if (strncmp(mode, "rb", 2) == 0) {
-    out = _wfopen(buf, L"rb");
-  } else if (strncmp(mode, "wb", 2) == 0) {
-    out = _wfopen(buf, L"wb");
-  }
+  MultiByteToWideChar(CP_UTF8, 0, path, -1, buf, len);
+  out = _wfopen(buf, mode_w);
 #else
-  out = fopen(txt, mode);
+  out = fopen(path, mode);
 #endif
 
   return out;
