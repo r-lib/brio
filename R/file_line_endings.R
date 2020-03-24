47466931f47ab1cef723c9cbf0e16fc9da978a8e
Jim Hester <james.f.hester@gmail.com>
2020-03-24 16:07:32 -0400

    Add example for file_line_endings

--- R/file_line_endings.R
+++ R/file_line_endings.R
@@ -6,6 +6,16 @@
 #'   - '\\r\\n' - if the file uses Windows line endings
 #'   - NA - if it cannot be determined
 #' @export
+#' @examples
+#' tf1 <- tempfile()
+#' tf2 <- tempfile()
+#' write_lines("foo", tf1, eol = "\n")
+#' write_lines("bar", tf2, eol = "\r\n")
+#'
+#' file_line_endings(tf1)
+#' file_line_endings(tf2)
+#'
+#' unlink(c(tf1, tf2))
 file_line_endings <- function(path) {
   path <- normalizePath(path, mustWork = TRUE)
   .Call(brio_file_line_endings, path)
