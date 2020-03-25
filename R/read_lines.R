14def780d571ad48d5a95c7247dd000e416b40c9
Jim Hester <james.f.hester@gmail.com>
2020-03-25 10:07:04 -0400

    Documentation tweaks

--- R/read_lines.R
+++ R/read_lines.R
@@ -25,13 +25,14 @@ read_lines <- function(path, n = -1) {
 #' - Does not warn or skip embedded nulls, they will likely crash R.
 #' - Does not warn if the file is missing the end of line character.
 #' - The arguments `ok`, `warn`, `encoding` and `skipNul` are ignored, with a warning.
-#' @param con A character string of the path to the file to read. Throws an error if a connection object is passed.
+#' @param con A character string of the path to a file. Throws an error if a connection object is passed.
 #' @inheritParams read_lines
 #' @param ok Ignored, with a warning.
 #' @param warn Ignored, with a warning.
 #' @param encoding Ignored, with a warning.
 #' @param skipNul Ignored, with a warning.
 #' @export
+#' @return A UTF-8 encoded character vector of the lines in the file.
 #' @examples
 #' authors_file <- file.path(R.home("doc"), "AUTHORS")
 #' data <- readLines(authors_file)
@@ -41,7 +42,7 @@ read_lines <- function(path, n = -1) {
 #' try(readLines(con))
 #' close(con)
 #'
-#' # Trying to use the unsupported args throws a warning
+#' # Trying to use unsupported args throws a warning
 #' data <- readLines(authors_file, encoding = "UTF-16")
 readLines <- function(con, n = -1, ok, warn, encoding, skipNul) {
   if (!is.character(con)) {
--- R/write_lines.R
+++ R/write_lines.R
@@ -3,7 +3,7 @@
 #' The text is converted to UTF-8 encoding before writing.
 #' @param text A character vector to write
 #' @param path A character string giving the file path to write to.
-#' @param eol The end of line character to use between lines.
+#' @param eol The end of line characters to use between lines.
 #' @return The UTF-8 encoded input text (invisibly).
 #' @export
 #' @examples
@@ -25,9 +25,28 @@ write_lines <- function(text, path, eol = "\n") {
 #' functionality. Compared to [base::writeLines()] it:
 #' - Only works with file paths, not connections.
 #' - Uses [enc2utf8()] to convert `text()` to UTF-8 before writing.
-#' - Uses `sep` uncoditionally as the line ending, regardless of platform.
+#' - Uses `sep` unconditionally as the line ending, regardless of platform.
 #' - The `useBytes` argument is ignored, with a warning.
+#' @inheritParams write_lines
+#' @inheritParams readLines
+#' @param sep The end of line characters to use between lines.
+#' @param useBytes Ignored, with a warning.
+#' @return The UTF-8 encoded input text (invisibly).
 #' @export
+#' @examples
+#' tf <- tempfile()
+#'
+#' writeLines(rownames(mtcars), tf)
+#'
+#' # Trying to use connections throws an error
+#' con <- file(tf)
+#' try(writeLines(con))
+#' close(con)
+#'
+#' # Trying to use unsupported args throws a warning
+#' writeLines(rownames(mtcars), tf, useBytes = TRUE)
+#'
+#' unlink(tf)
 writeLines <- function(text, con, sep = "\n", useBytes) {
   if (!is.character(con)) {
     stop("Only file paths are supported by brio::writeLines()", call. = FALSE)
