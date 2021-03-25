81e576ac798769c849290b299c253e3566ad6f35
Michael Quinn <msquinn@google.com>
2021-03-25 14:48:06 -0700

    Still provide support for connections.

--- R/writeLines.R
+++ R/writeLines.R
@@ -6,6 +6,11 @@
 #' - Uses [enc2utf8()] to convert `text()` to UTF-8 before writing.
 #' - Uses `sep` unconditionally as the line ending, regardless of platform.
 #' - The `useBytes` argument is ignored, with a warning.
+#'
+#' Because of thee features, `writeLines()` can only work with text file
+#' paths. If a connection is provided, it will fall back to
+#' [base::writeLines()].
+#'
 #' @inheritParams write_lines
 #' @inheritParams readLines
 #' @param sep The end of line characters to use between lines.
@@ -18,7 +23,7 @@
 #'
 #' writeLines(rownames(mtcars), tf)
 #'
-#' # Trying to use connections throws an error
+#' # Trying to use connections throws a warning, using base::writeLines
 #' con <- file(tf)
 #' try(writeLines(con))
 #' close(con)
@@ -27,7 +32,20 @@
 #' writeLines(rownames(mtcars), tf, useBytes = TRUE)
 #'
 #' unlink(tf)
-writeLines <- function(text, con = stdout(), sep = "\n", useBytes) {
+writeLines <- function(text, con = stdout(), sep = "\n", useBytes = FALSE) {
+  UseMethod("writeLines", con)
+}
+
+#' @describeIn writeLines Use base::writeLines for connections
+#' @export
+writeLines.default <- function(text, con = stdout(), sep = "\n", useBytes = FALSE) {
+  warning("`con` is a connection. Falling back to base::writeLines().")
+  base::writeLines(text, con, sep, useBytes)
+}
+
+#' @describeIn writeLines Use brio::writeLines for character file names
+#' @export
+writeLines.character <- function(text, con, sep = "\n", useBytes) {
   if (!is.character(con)) {
     stop("Only file paths are supported by brio::writeLines()", call. = FALSE)
   }
