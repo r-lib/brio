16c71f5ac2c0631b7acc6e3360fb1d21eb15f90a
Gábor Csárdi <csardi.gabor@gmail.com>
2023-10-31 15:00:54 +0100

    Match R and test files

--- tests/testthat/test-write_lines.R
+++ tests/testthat/test-write_lines.R
@@ -55,3 +55,26 @@ test_that("writeLines works", {
   # Warns if you use useBytes
   expect_warning(writeLines("foo\nbar\n", tmp, useBytes = TRUE), "is ignored")
 })
+
+test_that("UTF-8 file names", {
+  skip_on_cran()
+  tmp <- tempfile()
+  on.exit(unlink(tmp, recursive = TRUE), add = TRUE)
+  dir.create(tmp)
+
+  str <- "xx\xfcxx"
+  Encoding(str) <- "latin1"
+
+  normalizePath(str, mustWork = FALSE)
+
+  path <- file.path(tmp, str)
+  Encoding(path) <- "latin1"
+
+  write_lines(str, path)
+  expect_true(file.exists(path))
+  str2 <- read_lines(path)
+  expect_identical(
+    charToRaw(enc2utf8(str2)),
+    charToRaw(str2)
+  )
+})
